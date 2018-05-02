c1f1ea0d5e1a119cc138189465bd31d1b7c45ad6
Aravind <Aravind@DESKTOP-4RSCUV3.(none)>
2018-05-02 09:09:41 +0530

    my first commit

---
+++ client/app/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import '../assets/css/main.scss';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Bring Me Thanoss...!!!</h1>
+            </div>
+        );
+
+    }
+}
+
+
+render(<App/>, document.getElementById('app'));
