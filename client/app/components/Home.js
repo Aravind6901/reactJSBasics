64bf678e47dde58eaf903f719b7ec5124695aefa
Aravind <Aravind@DESKTOP-4RSCUV3.(none)>
2018-05-13 13:09:51 +0530

    thor and his friends

--- client/app/components/Home.js
+++ client/app/components/Home.js
@@ -4,7 +4,15 @@ export class Home extends React.Component {
     render() {
         return (
             <div>
-                <p>In A New Component</p>
+                <p>In A New Component Of Home Page</p>
+                <p>Mighty {this.props.name} and his age is {this.props.age}</p>
+                <p>Thor Came with his new {this.props.user.name} </p>
+                <div>
+                    <h4>Thor Also has his new Friends with him.</h4>
+                    <ul>
+                        {this.props.user.friends.map((friends,i)=> <li key={i}>{friends}</li>)}
+                    </ul>
+                </div>
             </div>
         );
     }
--- client/app/index.js
+++ client/app/index.js
@@ -7,6 +7,12 @@ import '../assets/css/main.scss';
 class App extends React.Component {
 
     render() {
+        var user = {
+            name: "StromBreaker",
+            age: 1,
+            friends:["Rakoon", "Groot"]
+
+        };
         return (
             <div className="container">
                 <div className="row">
@@ -14,9 +20,10 @@ class App extends React.Component {
                         <Header />
                     </div>
                 </div>
+                <p>in between</p>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home />
+                        <Home name={"Thor"} age={1500} user={user} />
                     </div>
                 </div>
             </div>
